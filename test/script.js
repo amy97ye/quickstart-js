6c97a9bd17464918ca47371041a78d18fabd20e7
amy97ye <amye@ucdavis.edu>
2017-03-10 17:08:37 -0800

    fadsf

--- test/script.js
+++ test/script.js
@@ -3,9 +3,7 @@ window.addEventListener('load',function(){
   var signInButton = document.getElementById("sign-in-button");
   signInButton.addEventListener('click',function(){
     var provider = new firebase.auth.GoogleAuthProvider();
-
     firebase.auth().signInWithPopup(provider);
-
   });
 
 });
